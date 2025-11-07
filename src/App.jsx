7c326dec78f442e7c1f7a2fc7c9e6ca88d373237
yvdzke . <yudzke@gmail.com>
2025-11-07 11:56:06 +0700

    Create Pages Folder and Home Page Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,12 @@
 import { useState } from "react";
 
+// Pages Import
+import Home from "./pages/home.jsx";
+
 function App() {
   return (
     <>
-      <div className="flex items-center justify-center h-screen">
-        <div className="text-center">
-          <h1 className="text-4xl font-bold mb-4">
-            Hello, Vite + React + Tailwind CSS!
-          </h1>
-        </div>
-      </div>
+      <Home></Home>
     </>
   );
 }
---
+++ src/pages/home.jsx
@@ -0,0 +1,15 @@
+function Home() {
+  return (
+    <>
+      <div className="flex items-center justify-center h-screen">
+        <div className="text-center">
+          <h1 className="text-4xl font-bold mb-4">
+            Hello, Vite + React + Tailwind CSS!
+          </h1>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Home;
