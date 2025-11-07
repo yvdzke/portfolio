8005f9eb77a4951eb32932dfb84e892c615a585a
yvdzke . <yudzke@gmail.com>
2025-11-07 11:50:27 +0700

    First commit message

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+
+function App() {
+  return (
+    <>
+      <div className="flex items-center justify-center h-screen">
+        <div className="text-center">
+          <h1 className="text-4xl font-bold mb-4">
+            Hello, Vite + React + Tailwind CSS!
+          </h1>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
