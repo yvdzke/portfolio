caa2584f9f2bf77688d004c61373296434a6adb6
yvdzke . <yudzke@gmail.com>
2025-11-07 12:51:02 +0700

    styling nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,7 +1,7 @@
 function Nav() {
   return (
     <>
-      <nav className="fixed top-4 left-1/2 -translate-x-1/2 z-50 bg-[#12171F]/80 backdrop-blur-md border border-gray-700 rounded-2xl px-7 py-3 flex justify-between items-center w-[90%] max-w-4xl shadow-md">
+      <nav className="fixed top-4 left-1/2 -translate-x-1/2 z-50 bg-[#12171F]/80 backdrop-blur-md border border-gray-700 rounded-lg px-7 py-3 flex justify-between items-center w-[90%] max-w-4xl shadow-md">
         {/* Logo */}
         <div className="text-white text-2xl font-semibold tracking-wide select-none">
           V
